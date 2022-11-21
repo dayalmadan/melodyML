a8d8684738d2c9d9fb244a1b66f688d7aa9bdf7d
kaverikhaneja19 <kaverikhaneja19@gmail.com>
2022-11-21 20:18:07 +0000

    resolved review comments
    Signed-off-by: kaverikhaneja19 <kaverikhaneja19@gmail.com>

--- frontend/src/components/Description.tsx
+++ frontend/src/components/Description.tsx
@@ -2,7 +2,7 @@ const Description = () => {
     return (
         <div className="description">
             <p>
-                Generate techno & house melodies in a few clicks. Choose from
+                Generate house & techno melodies in a few clicks. Choose from
                 the following options and Let the Music Begin.
             </p>
         </div>
--- frontend/src/components/TempoSlider.jsx
+++ frontend/src/components/TempoSlider.jsx
@@ -5,16 +5,20 @@ import { sliderTheme } from '../styles/sliderTheme';
 
 export const TempoSlider = () => {
     const [tempoValue, setTempoValue] = useState(120);
+
+    const defaultTempo = 120;
+    const maxTempo = 180;
+    const minTempo = 60;
     return (
         <div className="tempo-container">
             <div className="tempo-text">Tempo: {tempoValue} bpm</div>
             <ThemeProvider theme={sliderTheme}>
                 <Slider
                     aria-label="Tempo"
-                    defaultValue={120}
+                    defaultValue={defaultTempo}
                     valueLabelDisplay="off"
-                    min={60}
-                    max={180}
+                    min={minTempo}
+                    max={maxTempo}
                     onChange={(e, value) => {
                         setTempoValue(value);
                     }}
